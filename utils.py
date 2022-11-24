74b5e1c8047b5c99f5f50beb8755b6a668025469
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-11-24 17:37:41 -0500

    refactor - create reqs - create readme - create file structure - create dynamic run option with parameters

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
 import sys
 
-# Helper function for reading file input
-def get_input(filename_in):
+
+def read_input(filename_in: str):
+    """
+    Reads graphs from the file and creates adjacency lists.
+
+    Args:
+        filename_in: file name
+
+    Returns:
+        adjacency_lists
+
+    """
+
     # Read the lines from the file
-    in_file = open(filename_in, 'r')
+    in_file = open(filename_in, "r")
     lines = in_file.readlines()
     in_file.close()
 
@@ -42,7 +53,7 @@ def get_input(filename_in):
             graph[i] = []
         # Add edges
         for i in range(num_edges):
-            edge = prob[i+1]
+            edge = prob[i + 1]
             graph[edge[0]].append(edge[1])
             graph[edge[1]].append(edge[0])
         # For sanity, sort each adjacency list
@@ -51,7 +62,7 @@ def get_input(filename_in):
 
         # Store graph to list
         problem_list.append(graph)
-        
+
     # Return list of problems
     return problem_list
 
@@ -69,13 +80,13 @@ def write_output(data_out, filename_out):
             if len(prob[vert]) == 1:
                 leaves += 1
             edges += len(prob[vert])
-        edges //= 2 # We count each edge twice, so divide by 2
+        edges //= 2  # We count each edge twice, so divide by 2
 
         # Remove duplicate edges
         for vert in prob:
             for item in prob[vert]:
                 prob[item].remove(vert)
-        
+
         # Put everything in the list for output
         all_data.append((leaves, edges))
         for vert in prob:
@@ -87,74 +98,11 @@ def write_output(data_out, filename_out):
     for line in all_data:
         all_lines.append("{} {}\n".format(line[0], line[1]))
     all_lines[-1] = all_lines[-1].strip()
-    
+
     # Write output
-    out_file = open(filename_out, 'w')
+    out_file = open(filename_out, "w")
     out_file.writelines(all_lines)
-    out_file.close
-
-
-# Simple greedy approach to the problem
-# Likely to be used as a baseline
-def simple_greedy(prob_in):
-    # Create a copy of the problem and get some metadata
-    prob_copy = prob_in.copy()
-    num_verts = len(prob_copy)
-    # Create an empty tree to build
-    tree = {}
-    for i in range(num_verts):
-        tree[i] = []
-    in_tree = [False for i in range(num_verts)]
-    # Need a list of candidates
-    candidates = []
-
-    # Find the root node
-    root_vert = 0
-    root_ecount = len(prob_copy[root_vert])
-    for i in range(1, num_verts):
-        if len(prob_copy[i]) > root_ecount:
-            root_vert = i
-            root_ecount = len(prob_copy[i])
-    candidates.append(root_vert)
-    
-    # Keep adding to tree until we run out
-    while len(candidates) > 0:
-        # Get ideal candidate and remove from list
-        best = candidates[0]
-        best_ecount = len(prob_copy[best])
-        for i in candidates:
-            if len(prob_copy[i]) > best_ecount:
-                best = i
-                best_ecount = len(prob_copy[i])
-        candidates.remove(best)
-        #print("Ideal candidate: {}".format(best))
-
-        # Add ideal candidate's children to tree and list of candidates
-        best_elist = prob_copy[best]
-        in_tree[best] = True
-        for item in best_elist:
-            if not in_tree[item]:
-                in_tree[item] = True
-                tree[best].append(item)
-                tree[item].append(best)
-                candidates.append(item)
-        #print("Candidate list 1: {}".format(candidates))
-
-        # Prune candidates with no valid children
-        c_copy = candidates.copy()
-        for item in c_copy:
-            adj_list = prob_copy[item]
-            has_valid = False
-            for child in adj_list:
-                if not in_tree[child]:
-                    has_valid = True
-                    continue
-            if not has_valid:
-                candidates.remove(item)
-        #print("Candidate list 2: {}".format(candidates))
-
-    # Return the spanning tree
-    return tree
+    out_file.close()
 
 
 # Main function here
@@ -163,8 +111,8 @@ if __name__ == "__main__":
     print_input = False
     print_output = False
     # Set filenames for input and output
-    file_in = "./hard.in"
-    file_out = "./hard.out"
+    file_in = "inputs_outputs/hard.in"
+    file_out = "inputs_outputs/hard.out"
     if len(sys.argv) > 1:
         file_in = sys.argv[1]
     if len(sys.argv) > 2:
